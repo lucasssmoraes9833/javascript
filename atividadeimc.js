60c09bf697fd2bf4e0b0956e87d52ff6b49c1773
lucasssmoraes9833 <165032734+lucasssmoraes9833@users.noreply.github.com>
2024-08-28 08:25:45 -0300

    Add files via upload

--- atividadeimc.js
+++ atividadeimc.js
@@ -1,23 +1,24 @@
-const peso= 80
-const altura= 1.75
+const peso= 68
+const altura= 1.72
 
 const imc= (peso/(altura*altura))
+console.log(imc)
 
 if (imc < 16){
 console.log ("Magreza grave")
 }
-else if(imc < 16 && imc >= 16.9){
+else if(imc >= 16 && imc <= 16.9){
     console.log ("Magreza moderada")
 }
-else if ( imc <16.9 && imc >= 18.5 ){
+else if ( imc >=16.9 && imc <= 18.5 ){
 console.log ("Magreza leve")
 }
-else if ( imc < 18.5 && imc >= 24.9){
+else if ( imc >=18.5 && imc <= 24.9){
     console.log ("Peso ideal")
 }
-else if ( imc < 24.9 && imc >= 29.9){
+else if ( imc >= 24.9 && imc <= 29.9){
     console.log ("Sobrepeso")
 }
-else if (imc < 29.9 && imc >= 34.9){
+else if (imc >= 29.9 && imc <= 34.9){
     console.log ("Obesidade grau I")
 }
